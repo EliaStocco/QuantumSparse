d04936288110bdc890c953dc408d9ef643ec6a67
EliaStocco <eliastocco@gmail.com>
2024-08-16 21:34:55 +0200

    prepared example

--- examples/Ising-radial.py
+++ examples/Ising-radial.py
@@ -1,59 +1,49 @@
-
-import sys,os
-dir_ = os.path.dirname(__file__)
-os.chdir(dir_)
-print("\n\tchanging working directory to:",dir_)
-sys.path.append("..")
-import QuantumSparse as qs
+from QuantumSparse.spin import SpinOperators
+from QuantumSparse.operator import Operator
 import numpy as np
-import matplotlib.pyplot as plt
-
 
 S     = 2
 NSpin = 4
 spin_values = np.full(NSpin,S)
 
+# construct the spin operators
+SpinOp = SpinOperators(spin_values)
+# unpack the operators
+Sx,Sy,Sz = SpinOp.Sx,SpinOp.Sy,SpinOp.Sz
+# Sx is an array of length NSpin, 
+print("\tSx.shape = ",Sx.shape)
+# each element is a sparse matrix
+# with the Sx operator acting on the corresponding site
+print("\tSx[0].shape = ",Sx[0].shape)
+# You will see that the shape of the operator mathces the size of the Hilbert space
 
-Sx,Sy,Sz = qs.compute_spin_operators(spin_values)
-
-# Hamiltonian 
+# Good, these are your spin operators.
+# Now you can build any operator that you want
+# Let's build an Hamiltonian with radial 1nn interactions 
 H =  Sy[0]@Sx[1] - Sx[1]@Sy[2] + Sy[2]@Sx[3] - Sx[3]@Sy[0] 
-E0,Psi = qs.diagonalize_Hamiltonian(H,NLanczos=20,tol=1E-8,MaxDim=100)
+
+# Just for static typing reason ... 
+print("\ttype(H):",type(H))
+H = Operator(H)
+# Let's have a look at the Hamiltonian
+repr(H) # better than print(H)
+
+# Diagonalize!
+# YOU CAN DECOMMENT THESE LINES
+# print()
+# E0,Psi = H.diagonalize(method="dense") #),NLanczos=20,tol=1E-8,MaxDim=100)
+
+# or let's do with our favorite numerical routine
+Hdense = np.asarray(H.todense()) # let's convert H from a scipy.sparse matrix into a normal np.array
+print("\ttype(Hdense):",type(Hdense))
+E0,Psi = np.linalg.eigh(Hdense)
+
 E0 = E0.real
 E0.sort()
+
+print("\tmin eigenvalue:",E0[0])
+print("\tmax eigenvalue:",E0[-1])
 E0 = E0-min(E0)
-#print(E)
+print("\tenergy range:",E0[-1]-E0[0])
 
-
-
-Sx,Sy,Sz = qs.compute_spin_operators(spin_values)
-EulerAngles = np.asarray([[0,0,0],\
-               [0,0,-90],\
-               [0,0,180],\
-               [0,0,90]])    
-
-EulerAngles = np.pi * EulerAngles / 180                     
-St,Sr,Sz= qs.rotate(EulerAngles,Sx,Sy,Sz)
-
-# Hamiltonian 
-#H = Sr[0]@Sr[1] + Sr[1]@Sr[2] + Sr[2]@Sr[3] + Sr[3]@Sr[0] 
-H = qs.Ising(Sr)
-E1,Psi = qs.diagonalize_Hamiltonian(H,NLanczos=20,tol=1E-8,MaxDim=100)
-
-
-fig,((ax0),(ax1)) = plt.subplots(2,1,figsize=(10,10),sharex=True)
-axes = [ax0,ax1]
-
-axes[0].vlines(E0,0,1,color="blue")
-axes[0].grid(True)
-axes[0].set_xlabel("energy [eV]")
-axes[0].set_yticks([],[])
-axes[0].set_title("Ising Hamiltonian: 1D chain with radial spin interactions only")
-
-axes[1].vlines(E1,0,1,color="red")
-axes[1].grid(True)
-axes[1].set_xlabel("energy [eV]")
-axes[1].set_yticks([],[])
-#axes[1].set_title("Heisenberg Hamiltonian")
-
-plt.show()
+pass
