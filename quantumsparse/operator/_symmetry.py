d2464d946e3a4b231f430f6f66a39b6fb7ccfc13
Elia Stocco <elia.stocco@mpsd.mpg.de>
2025-06-28 17:24:20 +0200

    checking eigenvector normalization

--- quantumsparse/operator/_symmetry.py
+++ quantumsparse/operator/_symmetry.py
@@ -11,38 +11,25 @@ class Symmetry(Operator):
     #     else:
     #         self = Op.copy()
     
-    def set_eigenvalues(self:T,w):
+    def set_eigen(self:T,w:np.ndarray,f:np.ndarray):
         if self.shape[0] != len(w):
             raise ValueError("worng number of eigenvalues")
         elif not np.allclose(np.abs(w),1):
             raise ValueError("not all eigenvalues have modulus 1")
         else:
-            self.eigenvalues = w            
+            self.eigenvalues = w   
         
-    def levels2eigenstates(self:T,energy_levels):
-        I = self.identity(self.shape[0])
-        for w in energy_levels:
-            M = Operator(self - I * w)
-            f = M.kernel()
-        pass
+        assert self.shape == f.shape, "error"
+        self.eigenstates = f
+        self.normalize_eigevecs()
+        return self.test_eigensolution()         
+        
+    # def levels2eigenstates(self:T,energy_levels):
+    #     I = self.identity(self.shape[0])
+    #     for w in energy_levels:
+    #         M = Operator(self - I * w)
+    #         f = M.kernel()
+    #     pass
     
 import cmath
 
-def roots_of_unity(N:int)->np.ndarray:
-    """
-    Calculate the N-th roots of unity.
-    
-    Parameters:
-        N (int): The degree of the roots of unity.
-    
-    Returns:
-        list: A list of the N-th roots of unity as complex numbers.
-    """
-    roots = np.zeros(N, dtype=complex)
-    for k in range(N):
-        # Calculate the k-th root of unity
-        angle = 2 * cmath.pi * k / N
-        root = cmath.exp(1j * angle)
-        roots[k] = root
-    
-    return roots
