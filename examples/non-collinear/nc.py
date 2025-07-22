edaba79f22e583ac6cad0f17de11baa1130ec3ce
Elia Stocco <elia.stocco@mpsd.mpg.de>
2025-07-22 13:14:51 +0200

    logfile

--- examples/non-collinear/nc.py
+++ examples/non-collinear/nc.py
@@ -17,7 +17,7 @@ logging.basicConfig(
     level=logging.DEBUG if DEBUG else logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(LOGFILE)
 
 # %% [markdown]
 # ## Parameters
