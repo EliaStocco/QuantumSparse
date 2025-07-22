f084d1c60aa1ccbd7ad9c79a66503b5d2a3f4f91
Elia Stocco <elia.stocco@mpsd.mpg.de>
2025-07-22 11:51:33 +0200

    cleaning

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='quantumsparse',
-      version='0.0',
-      description='A scipy.sparse based package to represent quantum spin operators',
-      packages=['quantumsparse'],
-      author_email='eliastocco@gmail.com',
-      zip_safe=False)
