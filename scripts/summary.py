b28755a4e03f664f5a56a2b2f6829cbc154ec668
Elia Stocco <elia.stocco@mpsd.mpg.de>
2026-01-06 22:03:44 +0100

    new sripts

---
+++ scripts/summary.py
@@ -0,0 +1,36 @@
+import argparse
+import json
+from quantumsparse.operator import Operator
+from quantumsparse.tools.mathematics import roots_of_unity
+from quantumsparse.spin.shift import shift
+from quantumsparse.conftest import NS2Ops
+
+def main(args):
+    
+    print("\n=== Summary of an operator ===\n")
+    
+    print(f"Reading operator from file '{args.input}' ... ", end="")
+    H = Operator.load(args.input)
+    print("done.")
+    
+    print("\nSummary:")
+    print(repr(H))
+    
+    if args.plot is not None:
+        options = json.loads(args.options) if args.options is not None else {}
+        options["file"] = args.plot
+        print(f"Plotting operator to file '{args.plot}' ... ", end="")
+        print(options)
+        H.visualize(**options)
+        print("done.")
+    
+    print("Job done :)\n")
+    
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Diagonalize the shift operator.")
+    parser.add_argument("-i", "--input"   , type=str, required=True , help="pickle input file.")
+    parser.add_argument("-p", "--plot"    , type=str, required=False, help="output plot file.", default=None)
+    parser.add_argument("-o", "--options" , type=str, required=False, help="JSON formatted options.", default=None)
+    args = parser.parse_args()
+    main(args)
