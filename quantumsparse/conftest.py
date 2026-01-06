a5a29e56330e8aacc5a8011dde912388e22b310b
Elia Stocco <elia.stocco@mpsd.mpg.de>
2026-01-06 20:55:30 +0100

    mixed interactions with random couplings

--- quantumsparse/conftest.py
+++ quantumsparse/conftest.py
@@ -13,21 +13,23 @@ parametrize_S           = pytest.mark.parametrize("S", [0.5, 1.0, 1.5, 2.0])
 TOLERANCE = 1e-10
 COUPLINGS = [1.0,2.1,3.5] # just some numbers
 
-
 from typing import List, Tuple
 import numpy as np
 from quantumsparse.spin import SpinOperators
 from quantumsparse.operator import Operator
 from quantumsparse.spin import Heisenberg, Dzyaloshinskii_Moriya, biquadratic_Heisenberg, anisotropy, rhombicity
 
+def random_couplings()->List[float]:
+    return np.random.rand(3)
+
 def get_H(Sx:List[Operator],Sy:List[Operator],Sz:List[Operator],interaction:str)->Operator:
     H = 0 
     if "heisenberg" in interaction:
-        H += Heisenberg(Sx=Sx, Sy=Sy, Sz=Sz,couplings=COUPLINGS)
+        H += Heisenberg(Sx=Sx, Sy=Sy, Sz=Sz,couplings=random_couplings())
     if "DM" in interaction:
-        H += Dzyaloshinskii_Moriya(Sx=Sx, Sy=Sy, Sz=Sz,couplings=COUPLINGS)
+        H += Dzyaloshinskii_Moriya(Sx=Sx, Sy=Sy, Sz=Sz,couplings=random_couplings())
     if "biquadratic" in interaction:
-        H += biquadratic_Heisenberg(Sx=Sx, Sy=Sy, Sz=Sz,couplings=COUPLINGS)
+        H += biquadratic_Heisenberg(Sx=Sx, Sy=Sy, Sz=Sz,couplings=random_couplings())
     if "anisotropy" in interaction:
         H += anisotropy(Sz=Sz)
     if "rhombicity" in interaction:
